d25fc78e438a4c66af2046190c408519833cf008
David Olagookun <erijesudo@gmail.com>
2023-06-22 09:31:01 +0100

    Initial Commit

---
+++ Peoples/PeoplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeoplesApp.swift
+//  Peoples
+//
+//  Created by David OH on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PeoplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
